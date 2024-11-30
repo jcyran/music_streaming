e2e52ce9cf4324d5914637e3142b3564568add8e
jcyran <cyran.jakub03@gmail.com>
2024-11-30 13:44:09 +0100

    batman

---
+++ src/main/java/org/mj/audio/IpProjektApplication.java
@@ -0,0 +1,13 @@
+package org.mj.audio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpProjektApplication.class, args);
+	}
+
+}
